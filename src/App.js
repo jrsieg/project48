cbbfccb3f1bc6002fd746fb38ab487e2f8e76d92
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2020-10-29 17:07:45 -0400

    kelly branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import './App.css';
+const baseURl = 'https://api.airvisual.com/v2/city?';
+const key = 'c3606135-b947-439f-8e8b-57ce6253a66a';
 
 
 function App() {
@@ -12,10 +13,21 @@ function App() {
       <div>nasa</div>
       <h4>return: satellite image </h4>
 
-      <div>weather</div>
-      <h4>return: weather</h4>
-    </div>
-  );
+      <h3>Weather</h3>
+      </div>
+
+  function fetchData {
+      e.preventDefault()
+      url = `${baseURl}city=Indianapolis&state=Indiana&country=USA&key=${key}`
+      fetch(url)
+        .then(result => result.json())
+        .then(jsonified => displayData(jsonified));
+    function displayData(jsonified) {
+      console.log(jsonified)
+    let pr=document.getElementById("pr")
+    precipitation = jsonified.data.current.weather.pr
+    pr.innerHTML = pr.innerHTML+ (`${precipitation}`);
+  }
 }
 
 export default App;
