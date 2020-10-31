02631e64314d4631bb3db1cef04359d85eaaaa60
Kelly Marie Nighbert <Kelly@Kellys-MacBook-Pro.local>
2020-10-30 20:25:37 -0400

    kelly2

--- src/components/apps/Weather.js
+++ src/components/apps/Weather.js
@@ -3,20 +3,6 @@ import React from 'react';
 const baseURl = 'https://openweathermap.org/';
 const key = '01c905cbdcf2eb6417b6745dc9318f11';
 
-// const weather = (props) => {
-//         url = `${baseURl}city=Indianapolis&state=Indiana&country=USA&key=${key}`
-//         fetch(url)
-//           .then(result => result.json())
-//           .then(jsonified => displayData(jsonified));
-//           console.log(json)
-//       function displayData(jsonified) {
-//         console.log(jsonified)
-//       let pr=document.getElementById("pr")
-//       precipitation = jsonified.data.current.weather.pr
-//       pr.innerHTML = pr.innerHTML+ (`${precipitation}`);
-//       }
-//     }
-
 function Weather (props) {
     const url = `${baseURl}api.openweathermap.org/data/2.5/weather?q={Indianapolis}&appid=${key}`
     fetch(url)
